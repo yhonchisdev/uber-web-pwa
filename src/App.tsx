1a74e966dbbab7afc06a63238037c0c6133a4bd1
Harold Yonathan Condori Challco <yhon53190@gmail.com>
2024-03-25 00:05:07 -0500

    Config and implementation of PWA - DONE

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App(): JSX.Element {
   return (
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
       <div className="flex items-center justify-center">
-        <div className="flex flex-row w-[430px] max-w-full h-screen overflow-y-hidden overflow-x-auto bg-white [&::-webkit-scrollbar]:hidden snap-x snap-mandatory sm:rounded-3xl sm:shadow-lg sm:shadow-black/20">
+        <div className="relative flex flex-row w-[430px] max-w-full h-screen overflow-y-hidden overflow-x-auto bg-white [&::-webkit-scrollbar]:hidden snap-x snap-mandatory sm:rounded-3xl sm:shadow-lg sm:shadow-black/20 sm:border-8 sm:border-gray-300">
           <div className="min-w-full snap-always snap-center">
             <StartTrip />
           </div>
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import Unfonts from "unplugin-fonts/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,5 +25,31 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "Pide un viaje | Uber Perú",
+        short_name: "Uber Perú",
+        description:
+          "Solicita un viaje aquí. Aprende qué es y cómo funciona la app de Uber en Perú",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
 });
