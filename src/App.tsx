1e1e806ec32dc77e8e897515cecc88b0fe0a5f26
Harold Yonathan Condori Challco <yhon53190@gmail.com>
2024-03-25 21:48:23 -0500

    Updated cover iphone - DONE

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,28 @@ import ConnectingToDriver from "@/screens/connecting-to-driver";
 import PreviousTrips from "@/screens/previous-trips";
 import SetPointOnMap from "@/screens/set-point-on-map";
 import Home from "@/screens/home";
+import iphone from "@/assets/iphone.png";
 
 function App(): JSX.Element {
   return (
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
       <div className="flex items-center justify-center">
-        <div className="relative flex flex-row w-[430px] max-w-full h-screen overflow-hidden bg-white sm:rounded-3xl sm:shadow-lg sm:shadow-black/20 sm:border-8 sm:border-gray-300">
-          <NavigatorProvider>
-            <Home />
-            <PlanYourTrip />
-            <SetPointOnMap />
-            <ConnectingToDriver />
-            <StartTrip />
-            <PreviousTrips />
-          </NavigatorProvider>
+        <div className="relative z-10 flex flex-row w-[430px] p-4 max-w-full h-screen ">
+          <img
+            className="absolute left-0 top-0 -z-10 w-screen h-screen"
+            src={iphone}
+            alt="iphone"
+          />
+          <div className="relative -z-10 w-full h-full rounded-[40px] overflow-hidden">
+            <NavigatorProvider>
+              <Home />
+              <PlanYourTrip />
+              <SetPointOnMap />
+              <ConnectingToDriver />
+              <StartTrip />
+              <PreviousTrips />
+            </NavigatorProvider>
+          </div>
         </div>
       </div>
     </APIProvider>
